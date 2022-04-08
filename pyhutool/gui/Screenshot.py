a9d04b825fc85a4e9cd4226a00c4a489cd6b8f1c
kaysen <xujiantao@nnthink.com>
2022-04-08 15:10:36 +0800

    兼容多显示器

--- pyhutool/gui/Screenshot.py
+++ pyhutool/gui/Screenshot.py
@@ -5,18 +5,23 @@ import subprocess
 import sys
 import functools
 import time
+import win32api
+import win32con
+import win32gui
+import win32ui
+from PIL import ImageGrab
 from pyhutool.gui.Const import _const
 
 try:
     from PIL import Image
     from PIL import ImageOps
     from PIL import ImageDraw
-    if sys.platform == 'win32': # TODO - Pillow now supports ImageGrab on macOS.
+
+    if sys.platform == 'win32':  # TODO - Pillow now supports ImageGrab on macOS.
         from PIL import ImageGrab
     _PILLOW_UNAVAILABLE = False
 except ImportError:
     _PILLOW_UNAVAILABLE = True
-
 
 scrotExists = False
 try:
@@ -26,15 +31,16 @@ try:
         scrotExists = whichProc.wait() == 0
 except OSError as ex:
     # if ex.errno == errno.ENOENT:
-        # if there is no "which" program to find scrot, then assume there
-        # is no scrot.
-        # pass
+    # if there is no "which" program to find scrot, then assume there
+    # is no scrot.
+    # pass
     # else:
     #     raise
     raise
 
 try:
     import cv2, numpy
+
     useOpenCV = True
     RUNNING_CV_2 = cv2.__version__[0] < '3'
 except ImportError:
@@ -47,6 +53,7 @@ def requiresPillow(wrappedFunction):
         if _PILLOW_UNAVAILABLE:
             raise Exception('The Pillow package is required to use this function.')
         return wrappedFunction(*args, **kwargs)
+
     return wrapper
 
 
@@ -73,7 +80,7 @@ def _screenshot_osx(imageFilename=None, region=None):
         assert len(region) == 4, 'region argument must be a tuple of four ints'
         region = [int(x) for x in region]
         im = im.crop((region[0], region[1], region[2] + region[0], region[3] + region[1]))
-        os.unlink(tmpFilename) # delete image of entire screen to save cropped version
+        os.unlink(tmpFilename)  # delete image of entire screen to save cropped version
         im.save(tmpFilename)
     else:
         # force loading before unlinking, Image.open() is lazy
@@ -82,6 +89,7 @@ def _screenshot_osx(imageFilename=None, region=None):
     if imageFilename is None:
         os.unlink(tmpFilename)
     return im
+
 
 def _screenshot_linux(scrotExists=False, imageFilename=None, region=None):
     if not scrotExists:
@@ -97,7 +105,7 @@ def _screenshot_linux(scrotExists=False, imageFilename=None, region=None):
             assert len(region) == 4, 'region argument must be a tuple of four ints'
             region = [int(x) for x in region]
             im = im.crop((region[0], region[1], region[2] + region[0], region[3] + region[1]))
-            os.unlink(tmpFilename) # delete image of entire screen to save cropped version
+            os.unlink(tmpFilename)  # delete image of entire screen to save cropped version
             im.save(tmpFilename)
         else:
             # force loading before unlinking, Image.open() is lazy
@@ -110,7 +118,6 @@ def _screenshot_linux(scrotExists=False, imageFilename=None, region=None):
         raise Exception('The scrot program must be installed to take a screenshot with PyScreeze on Linux. Run: sudo apt-get install scrot')
 
 
-
 RUNNING_PYTHON_2 = sys.version_info[0] == 2
 if useOpenCV:
     if RUNNING_CV_2:
@@ -121,7 +128,7 @@ if useOpenCV:
         LOAD_GRAYSCALE = cv2.IMREAD_GRAYSCALE
 
 if not RUNNING_PYTHON_2:
-    unicode = str # On Python 3, all the isinstance(spam, (str, unicode)) calls will work the same as Python 2.
+    unicode = str  # On Python 3, all the isinstance(spam, (str, unicode)) calls will work the same as Python 2.
 
 Box = collections.namedtuple('Box', 'left top width height')
 Point = collections.namedtuple('Point', 'x y')
@@ -159,12 +166,12 @@ def _locateAll_opencv(needleImage, haystackImage, grayscale=None, limit=10000, r
     haystackImage = _load_cv2(haystackImage, grayscale)
 
     if region:
-        haystackImage = haystackImage[region[1]:region[1]+region[3],
-                                      region[0]:region[0]+region[2]]
+        haystackImage = haystackImage[region[1]:region[1] + region[3],
+                        region[0]:region[0] + region[2]]
     else:
         region = (0, 0)  # full image; these values used in the yield statement
     if (haystackImage.shape[0] < needleImage.shape[0] or
-        haystackImage.shape[1] < needleImage.shape[1]):
+            haystackImage.shape[1] < needleImage.shape[1]):
         # avoid semi-cryptic OpenCV error below if bad size
         raise ValueError('needle dimension(s) exceed the haystack image or region dimensions')
 
@@ -241,7 +248,8 @@ def locateOnScreen(image, minSearchTime=0, **kwargs):
     start = time.time()
     while True:
         try:
-            screenshotIm = screenshot(region=None) # the locateAll() function must handle cropping to return accurate coordinates, so don't pass a region here.
+            screenshotIm = screenshot(
+                region=None)  # the locateAll() function must handle cropping to return accurate coordinates, so don't pass a region here.
             retVal = locate(image, screenshotIm, **kwargs)
             try:
                 screenshotIm.fp.close()
@@ -264,14 +272,14 @@ def _kmp(needle, haystack, _dummy):
     shifts = [1] * (len(needle) + 1)
     shift = 1
     for pos in range(len(needle)):
-        while shift <= pos and needle[pos] != needle[pos-shift]:
-            shift += shifts[pos-shift]
-        shifts[pos+1] = shift
+        while shift <= pos and needle[pos] != needle[pos - shift]:
+            shift += shifts[pos - shift]
+        shifts[pos + 1] = shift
     startPos = 0
     matchLen = 0
     for c in haystack:
         while matchLen == len(needle) or \
-              matchLen >= 0 and needle[matchLen] != c:
+                matchLen >= 0 and needle[matchLen] != c:
             startPos += shifts[matchLen]
             matchLen -= shifts[matchLen]
         matchLen += 1
@@ -320,9 +328,9 @@ def _locateAll_python(needleImage, haystackImage, grayscale=None, limit=None, re
     if region is not None:
         haystackImage = haystackImage.crop((region[0], region[1], region[0] + region[2], region[1] + region[3]))
     else:
-        region = (0, 0) # set to 0 because the code always accounts for a region
+        region = (0, 0)  # set to 0 because the code always accounts for a region
 
-    if grayscale: # if grayscale mode is on, convert the needle and haystack images to grayscale
+    if grayscale:  # if grayscale mode is on, convert the needle and haystack images to grayscale
         needleImage = ImageOps.grayscale(needleImage)
         haystackImage = ImageOps.grayscale(haystackImage)
     else:
@@ -339,11 +347,13 @@ def _locateAll_python(needleImage, haystackImage, grayscale=None, limit=None, re
     needleImageData = tuple(needleImage.getdata())
     haystackImageData = tuple(haystackImage.getdata())
 
-    needleImageRows = [needleImageData[y * needleWidth:(y+1) * needleWidth] for y in range(needleHeight)] # LEFT OFF - check this
+    needleImageRows = [needleImageData[y * needleWidth:(y + 1) * needleWidth] for y in range(needleHeight)]  # LEFT OFF - check this
     needleImageFirstRow = needleImageRows[0]
 
-    assert len(needleImageFirstRow) == needleWidth, 'For some reason, the calculated width of first row of the needle image is not the same as the width of the image.'
-    assert [len(row) for row in needleImageRows] == [needleWidth] * needleHeight, 'For some reason, the needleImageRows aren\'t the same size as the original image.'
+    assert len(
+        needleImageFirstRow) == needleWidth, 'For some reason, the calculated width of first row of the needle image is not the same as the width of the image.'
+    assert [len(row) for row in needleImageRows] == [
+        needleWidth] * needleHeight, 'For some reason, the needleImageRows aren\'t the same size as the original image.'
 
     numMatchesFound = 0
 
@@ -351,20 +361,20 @@ def _locateAll_python(needleImage, haystackImage, grayscale=None, limit=None, re
     # value greater than 1 does not give *any* significant performance improvements.
     # Since using a step higher than 1 makes for less accurate matches, it will be
     # set to 1.
-    step = 1 # hard-code step as 1 until a way to improve it can be figured out.
+    step = 1  # hard-code step as 1 until a way to improve it can be figured out.
 
     if step == 1:
         firstFindFunc = _kmp
     else:
         firstFindFunc = _steppingFind
 
-
-    for y in range(haystackHeight): # start at the leftmost column
-        for matchx in firstFindFunc(needleImageFirstRow, haystackImageData[y * haystackWidth:(y+1) * haystackWidth], step):
+    for y in range(haystackHeight):  # start at the leftmost column
+        for matchx in firstFindFunc(needleImageFirstRow, haystackImageData[y * haystackWidth:(y + 1) * haystackWidth], step):
             foundMatch = True
             for searchy in range(1, needleHeight, step):
                 haystackStart = (searchy + y) * haystackWidth + matchx
-                if needleImageData[searchy * needleWidth:(searchy+1) * needleWidth] != haystackImageData[haystackStart:haystackStart + needleWidth]:
+                if needleImageData[searchy * needleWidth:(searchy + 1) * needleWidth] != haystackImageData[
+                                                                                         haystackStart:haystackStart + needleWidth]:
                     foundMatch = False
                     break
             if foundMatch:
@@ -379,7 +389,6 @@ def _locateAll_python(needleImage, haystackImage, grayscale=None, limit=None, re
                         haystackFileObj.close()
                     return
 
-
     # There was no limit or the limit wasn't reached, but close the file handles anyway.
     if needleFileObj is not None:
         needleFileObj.close()
@@ -391,6 +400,136 @@ def _locateAll_python(needleImage, haystackImage, grayscale=None, limit=None, re
             raise Exception('Could not locate the image.')
         else:
             return
+
+
+def checkRect(rect):
+    try:
+        left, top, right, bottom = rect
+    except ValueError:
+        raise ValueError("%r is not a valid rect; must contain 4 ints" % (rect,))
+    if not all(isinstance(x, (int, int)) for x in rect):
+        raise ValueError("%r is not a valid rect; must contain 4 ints" % (rect,))
+    width = right - left
+    height = bottom - top
+    if width <= 0 or height <= 0:
+        raise ValueError("%r is not a valid rect; width and height must not be "
+                         "zero or negative" % (rect,))
+
+
+def getDisplayRects():
+    HANDLE_MONITOR, HDC_MONITOR, SCREEN_RECT = range(3)
+    tries = 150
+    lastRects = None
+    for _ in range(tries):
+        try:
+            monitors = win32api.EnumDisplayMonitors(None, None)
+        except SystemError:
+            lastRects = None
+        else:
+            for m in monitors:
+                m[HDC_MONITOR].Close()
+            rects = list(m[SCREEN_RECT] for m in monitors)
+            try:
+                for rect in rects:
+                    checkRect(rect)
+            except ValueError:
+                lastRects = None
+            else:
+                if rects == lastRects:
+                    return rects
+                else:
+                    lastRects = rects
+
+    raise Exception("Could not get stable rect information after %d tries; "
+                    "last was %r." % (tries, lastRects))
+
+
+def deleteDCAndBitMap(dc, bitmap):
+    dc.DeleteDC()
+    handle = bitmap.GetHandle()
+    # Trying to DeleteObject(0) will throw an exception; it can be 0 in the case
+    # of an untouched win32ui.CreateBitmap()
+    if handle != 0:
+        win32gui.DeleteObject(handle)
+
+
+def getVirtualScreenRect():
+    tries = 150
+    lastRect = None
+    for _ in range(tries):
+        left = win32api.GetSystemMetrics(win32con.SM_XVIRTUALSCREEN)
+        top = win32api.GetSystemMetrics(win32con.SM_YVIRTUALSCREEN)
+        width = win32api.GetSystemMetrics(win32con.SM_CXVIRTUALSCREEN)
+        height = win32api.GetSystemMetrics(win32con.SM_CYVIRTUALSCREEN)
+
+        right = left + width
+        bottom = top + height
+
+        rect = (left, top, right, bottom)
+        try:
+            checkRect(rect)
+        except ValueError:
+            lastRect = None
+        else:
+            if rect == lastRect:
+                return rect
+            else:
+                lastRect = rect
+
+    raise Exception("Could not get stable rect information after %d tries; "
+                    "last was %r." % (tries, lastRect))
+
+
+def getDCAndBitMap(saveBmpFilename=None, rect=None):
+    if rect is None:
+        try:
+            rect = getVirtualScreenRect()
+        except Exception as e:
+            raise Exception("Error during getVirtualScreenRect: " + str(e))
+    # rect is already checked
+    else:
+        checkRect(rect)
+
+    left, top, right, bottom = rect
+    width = right - left
+    height = bottom - top
+
+    hwndDesktop = win32gui.GetDesktopWindow()
+
+    # Retrieve the device context (DC) for the entire virtual screen.
+    hwndDevice = win32gui.GetWindowDC(hwndDesktop)
+    ##print("device", hwndDevice)
+    assert isinstance(hwndDevice, (int, int)), hwndDevice
+
+    mfcDC = win32ui.CreateDCFromHandle(hwndDevice)
+    try:
+        saveDC = mfcDC.CreateCompatibleDC()
+        saveBitMap = win32ui.CreateBitmap()
+        # Above line is assumed to never raise an exception.
+        try:
+            try:
+                saveBitMap.CreateCompatibleBitmap(mfcDC, width, height)
+            except (win32ui.error, OverflowError) as e:
+                raise Exception("Could not CreateCompatibleBitmap("
+                                "mfcDC, %r, %r) - perhaps too big? Error was: %s" % (width, height, e))
+            saveDC.SelectObject(saveBitMap)
+            try:
+                saveDC.BitBlt((0, 0), (width, height), mfcDC, (left, top), win32con.SRCCOPY)
+            except win32ui.error as e:
+                raise Exception("Error during BitBlt. "
+                                "Possible reasons: locked workstation, no display, "
+                                "or an active UAC elevation screen. Error was: " + str(e))
+            if saveBmpFilename is not None:
+                saveBitMap.SaveBitmapFile(saveDC, saveBmpFilename)
+        except:
+            deleteDCAndBitMap(saveDC, saveBitMap)
+            # Let's just hope the above line doesn't raise an exception
+            # (or it will mask the previous exception)
+            raise
+    finally:
+        mfcDC.DeleteDC()
+
+    return saveDC, saveBitMap
 
 
 if useOpenCV:
