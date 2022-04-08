a67f7787b64a19550e0db20c2b6e2bdcc011fd9e
kaysen <xujiantao@nnthink.com>
2022-04-08 17:03:12 +0800

    识别图片中人脸

--- pyhutool/core/Image.py
+++ pyhutool/core/Image.py
@@ -1,7 +1,6 @@
-
-# 显示图片函数
 from PIL import Image
-
+import cv2
+face_cascade=cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_alt.xml')
 
 def showImage(image, title=None):
     """
@@ -122,3 +121,10 @@ def watermarkImage(image, watermark, x, y):
     """
     image.paste(watermark, (x, y))
     return image
+
+
+# 识别图片中的人脸函数
+def face_detect(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    return faces
